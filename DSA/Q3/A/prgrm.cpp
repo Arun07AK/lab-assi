58fded0051cb4e521e9397fe5f547c0611b19390
ARUN AK <arunarjunan9083@gmail.com>
2025-08-13 09:02:16 +0530

    Update prgrm.cpp

--- DSA/Q3/A/prgrm.cpp
+++ DSA/Q3/A/prgrm.cpp
@@ -1,26 +1,32 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-int main() {
-    //binay search
-    int *A;
-    int size=10;
-    A=new int [size]{1,2,3,0,5,6,7,8,9,10};
+int linear(int key,int* A)
+{
     int len=10;
-    int h=len-1;
-    int l=0;
-    int mid=0;
-    int key=6;
-    for(int i=0;i<len-1;i++)
+    for(int i=0;i<len;i++)
     {
-        if(A[i+1]!=A[i]+1)
+        if(key==A[i])
         {
-            cout<<i+1<<"th index is missing "<< "it should be "<<A[i]+1<<endl;
-            return 0;
+            return i;
         }
     }
-    
-    
+    return -1;
+}
+int main() {
+    //using linear search
+    int *A;
+    int size=10;
+    A=new int [size]{1,2,3,5,6,7,8,9,10};
+    int n=10;
+    for(int i=1;i<n;i++)
+    {
+        if(linear(i,A)==-1)
+        {
+            cout<<"the missing value is "<<i<<"  it should be added in the index  "<<i-1<<endl;
+            break;
+        }
+    }
 
     return 0;
 }
