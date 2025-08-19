20118e9cffcaca037c7227f1125abd413170b328
ARUN AK <arunarjunan9083@gmail.com>
2025-08-19 11:07:21 +0530

    Update Q4.bash

--- OS/ASSIGNMENT 3/Q4.bash
+++ OS/ASSIGNMENT 3/Q4.bash
@@ -3,6 +3,15 @@
 echo "enter the the character to be checked "
 read c
 case "$c" in
-    "a")echo "yes";;
-    *)echo "default";;
+    "a")echo "VOWEL";;
+    "e")echo "VOWEL";;
+    "i")echo "VOWEL";;
+    "o")echo "VOWEL";;
+    "u")echo "VOWEL";;
+    "A")echo "VOWEL";;
+    "E")echo "VOWEL";;
+    "I")echo "VOWEL";;
+    "O")echo "VOWEL";;
+    "U")echo "VOWEL";;
+    *)echo "CONSONANT";;
 esac
