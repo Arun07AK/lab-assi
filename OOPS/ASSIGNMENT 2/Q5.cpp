95754d5a7a2808f8eba3de9adc7b9f0874d2d0c2
ARUN AK <arunarjunan9083@gmail.com>
2025-09-06 21:04:48 +0530

    Create Q5.cpp

---
+++ OOPS/ASSIGNMENT 2/Q5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+//static member funtions
+using namespace std;
+int count=50;
+class Rectangle
+{
+    private:
+        int l;
+        int b;
+    
+    public:
+        static int count;
+        Rectangle()
+        {
+            l=1;
+            b=1;
+            count++;
+        }
+        static int getCount()
+        {
+            return count;
+        }
+};
+int Rectangle::count ;
+
+int main()
+{
+    int count=999;
+    Rectangle r1;
+    Rectangle r2;
+    cout << "Local variable count: " << count << endl;              // 999
+    cout << "Static member count: " << Rectangle::count << endl;    // static member
+    cout << "Global variable count: " << ::count << endl;  
+    
+}
