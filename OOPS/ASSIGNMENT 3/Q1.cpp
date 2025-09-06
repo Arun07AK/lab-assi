eb03a75d8af98ffdc487a7e14703da9d7905fa47
ARUN AK <arunarjunan9083@gmail.com>
2025-09-06 21:41:30 +0530

    Create Q1.cpp

---
+++ OOPS/ASSIGNMENT 3/Q1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+    private:
+        int l;
+        int b;
+    public:
+        Rectangle()
+        {
+            l=0;
+            b =0;
+        }
+        Rectangle(int l=0,int b=0)
+        {
+            this->l=l;
+            this->b =b;
+        }
+        Rectangle(int c)
+        {
+            l=c;
+            b =c;
+        }
+        void Setl(int l)
+        {
+            this->l=l;
+        }
+        void Setb(int b)
+        {
+            this->b=b;
+        }
+        int area()
+        {
+            return l*b;
+        }
+        int Getl()
+        {
+            return l;
+        }
+        int Getb()
+        {
+            return b;
+        }
+        ~Rectangle()
+        {
+            cout << "Destructor called for rectangle " << l << "x" << b << endl;
+        }
+};
+int main()
+{
+        Rectangle arr[3] = {
+        Rectangle(),        // Default constructor
+        Rectangle(6, 4),    // Two parameters
+        Rectangle(5)        // Single parameter
+    };
+    
+    cout<<"r1  :"<<arr[0].area()<<endl;
+    cout<<"r2  :"<<arr[1].area()<<endl;
+    cout<<"r3  :"<<arr[2].area()<<endl;
+    
+}
