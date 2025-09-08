cf462049358537da75008c41456ae9a54714c3b6
ARUN AK <arunarjunan9083@gmail.com>
2025-09-08 10:36:08 +0530

    Update Q1.cpp

--- OOPS/ASSIGNMENT 3/Q1.cpp
+++ OOPS/ASSIGNMENT 3/Q1.cpp
@@ -1,3 +1,9 @@
+/*1.Create a class named 'Rectangle' with two data members- length and breadth and a function to calculate the area which is 'length*breadth'. The class has three constructors which are :
+(a) having no parameter - values of both length and breadth are assigned zero.
+(b) having two numbers as parameters - the two numbers are assigned as length and  
+     breadth respectively.
+(c) having one number as parameter - both length and breadth are assigned that number.
+Now, create objects of the 'Rectangle' class having none, one and two parameters and print their areas.*/
 #include <iostream>
 using namespace std;
 class Rectangle
