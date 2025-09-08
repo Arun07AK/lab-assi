2b9880af4da26dd2af8c923102396c7e30562e61
ARUN AK <arunarjunan9083@gmail.com>
2025-09-08 10:28:41 +0530

    Update Q3.cpp

--- OOPS/ASSIGNMENT 2/Q3.cpp
+++ OOPS/ASSIGNMENT 2/Q3.cpp
@@ -1,3 +1,8 @@
+/*3.Create code snippets that illustrates the use of scope resolution operator (: :) for the following uses:
+a.Member functions defined outside class
+b.To access a global variable when there is a local variable with same name
+c.To access a class’s static variables
+d.Including in-built libraries*/
 #include <iostream>
 
 int count=50;
